8c07c1d937661b84a4f60140d52d9da9146a3289
zooizooi <zooii.kut@gmail.com>
2024-11-14 15:58:31 +0100

    :camera_flash: Enhance camera functionality

--- src/Camera.ts
+++ src/Camera.ts
@@ -1,9 +1,15 @@
-import { PerspectiveCamera } from 'three';
+import { PerspectiveCamera, Vector3 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { KeyboardHandler, LocalStorage } from '@zooizooi/utils';
+import { KEY_DOWN, KeyDirection } from '@zooizooi/utils/modules/KeyboardHandler';
 import Debugger, { DebuggerFolder } from '@/Debugger';
 import onWindowResize from '@/hooks/onWindowResize';
 import onUpdate from '@/hooks/onUpdate';
 import Globals from '@/Globals';
+import { ButtonGridApi } from '@tweakpane/plugin-essentials';
+
+const DEFAULT_CAMERA_POSITION = new Vector3(0, 5, 0);
+const LOCAL_STORAGE_CAMERA_SETTINGS = 'camera-settings';
 
 export default class PortalCamera {
     public camera: PerspectiveCamera;
@@ -11,25 +17,51 @@ export default class PortalCamera {
     private debugger: DebuggerFolder;
 
     constructor() {
+        this.createKeyboardHandler();
         this.debugger = this.createDebugger();
         this.camera = this.createCamera();
         this.controls = this.createControls();
+        this.loadCameraPosition();
 
         // Hooks
         onUpdate(this, this.update);
         onWindowResize(this, this.resize);
     }
 
+    private createKeyboardHandler() {
+        const keymap = {
+            'Ctrl+s': this.keyCtrlSHandler,
+        };
+
+        return new KeyboardHandler({
+            scope: this,
+            keymap,
+        });
+    }
+
     private createDebugger() {
         const debug = Debugger.addFolder({ title: 'Camera' });
+        const positionButtonGrid = debug.addBlade({
+            view: 'buttongrid',
+            size: [2, 1],
+            cells: (x: number, y: number) => ({
+                title: [
+                    ['Store', 'Reset'],
+                ][y][x],
+            }),
+            label: 'position',
+        }) as ButtonGridApi;
+        positionButtonGrid.on('click', (event) => {
+            if (event.cell.title === 'Store') this.storeCameraPosition();
+            if (event.cell.title === 'Reset') this.resetCameraPosition();
+        });
         return debug;
     }
 
     private createCamera() {
         const camera = new PerspectiveCamera(50, 1);
-        camera.position.set(0, 0, 5);
+        camera.position.copy(DEFAULT_CAMERA_POSITION);
         this.debugger.addBinding(camera, 'fov', { min: 1, max: 180 }).on('change', () => camera.updateProjectionMatrix());
-        this.debugger.addBinding(camera, 'position');
         return camera;
     }
 
@@ -40,6 +72,32 @@ export default class PortalCamera {
         return undefined;
     }
 
+    private storeCameraPosition() {
+        LocalStorage.set(LOCAL_STORAGE_CAMERA_SETTINGS, {
+            position: this.camera.position,
+            target: this.controls?.target,
+        });
+        this.debugger.refresh();
+        console.log('📷 Camera position stored');
+    }
+
+    private loadCameraPosition() {
+        const data: any = LocalStorage.get(LOCAL_STORAGE_CAMERA_SETTINGS);
+        if (!data) return;
+        this.camera.position.set(data.position.x, data.position.y, data.position.z);
+        this.controls?.target.set(data.target.x, data.target.y, data.target.z);
+        this.debugger.refresh();
+        console.log('📷 Camera position loaded');
+    }
+
+    private resetCameraPosition() {
+        this.camera.position.copy(DEFAULT_CAMERA_POSITION);
+        this.controls?.target.set(0, 0, 0);
+        LocalStorage.remove(LOCAL_STORAGE_CAMERA_SETTINGS);
+        this.debugger.refresh();
+        console.log('📷 Camera position was reset');
+    }
+
     private update() {
         this.controls?.update();
     }
@@ -47,5 +105,12 @@ export default class PortalCamera {
     private resize() {
         this.camera.aspect = window.innerWidth / window.innerHeight;
         this.camera.updateProjectionMatrix();
+    }
+
+    private keyCtrlSHandler(direction: KeyDirection, event: KeyboardEvent) {
+        if (direction === KEY_DOWN) {
+            event.preventDefault();
+            this.storeCameraPosition();
+        }
     }
 }
--- src/Debugger.ts
+++ src/Debugger.ts
@@ -1,5 +1,7 @@
 import { FolderApi, Pane } from 'tweakpane';
+import * as EssentialsPlugin from '@tweakpane/plugin-essentials';
 
 const pane = new Pane();
+pane.registerPlugin(EssentialsPlugin);
 export type DebuggerFolder = FolderApi;
 export default pane;
