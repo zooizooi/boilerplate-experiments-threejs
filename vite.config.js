c85395f5e5cbd4c59565e0a648f0a882a67649b5
zooizooi <zooii.kut@gmail.com>
2025-02-03 11:59:46 +0100

    📦 Update dependencies & Switch to NPM

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
 import { resolve } from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
     resolve: {
@@ -10,5 +11,5 @@ export default defineConfig({
     },
     plugins: [glsl({
         compress: true,
-    })],
+    }), visualizer({ open: true })],
 });
